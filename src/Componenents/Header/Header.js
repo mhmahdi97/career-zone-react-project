9bd710c10b66ae6770ed72825b5b58d0a66e8cbb
mhmahdi97 <mhmahdi97@gmail.com>
2023-04-12 08:45:21 +0600

    created responsive navbar

--- src/Componenents/Header/Header.js
+++ src/Componenents/Header/Header.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false);
+    
+    const handleIcon = () => {
+        setOpen(!open)
+    }
+
     return (
         <div className='header'>
             <Link to="/" className="brand-title">Career Zone</Link>
-             <nav className='nav-list'>
+             <nav className={open === false ? 'nav-list' : 'nav-list open'}>
             <Link to="/">Home</Link>
             <Link to="/statistics">Statistics</Link>
             <Link to="/applied-jobs">Applied Jobs</Link>
             <Link to="/blog">Blog</Link>
-            </nav>
-            <div className='btn-icon-container'>
-                <div className='btn-container'>
-                <button className='apply-btn'>Start Applying</button>
-                </div>
-                <div className='menu-icon'>
-                    
-                </div>
+            <button className='apply-btn'>Start Applying</button>
+            <div className='btn-container'>
             </div>
+            </nav>
+            <div onClick={handleIcon} className='menu-icon'>
+                    <span>
+                        {
+                            open === true ? <FontAwesomeIcon icon={faX} /> 
+                            : <FontAwesomeIcon icon={faBars} />
+                        }
+                    </span>
+            </div>
+            {/* <div className='btn-icon-container'>
+                
+            </div> */}
         </div>
     );
 };
